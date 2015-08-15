4d1338cd1707b4ed61b3b7a6e6b435314aa3df47
Bart Prokop <prokop.bart@gmail.com>
2015-08-16 00:03:04 +0100

    Adding zajazd.biz

---
+++ zajazd.biz/xmlrpc0db0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://strona14.twojliderwbiznesie.pl</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://strona14.twojliderwbiznesie.pl/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://strona14.twojliderwbiznesie.pl/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://strona14.twojliderwbiznesie.pl/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://strona14.twojliderwbiznesie.pl/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
